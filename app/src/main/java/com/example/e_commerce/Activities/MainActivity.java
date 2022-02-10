fabbedc0274da8892f093003210802f75223e6b1
sajibsaha4647 <sajibsahabdtask@gmail.com>
2022-02-10 17:18:48 +0600

    updated

--- app/src/main/java/com/example/e_commerce/Activities/MainActivity.java
+++ app/src/main/java/com/example/e_commerce/Activities/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.e_commerce.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.example.e_commerce.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        viewPager = findViewById(R.id.introid);
+
+
     }
 }
